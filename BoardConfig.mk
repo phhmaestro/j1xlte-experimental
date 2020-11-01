3a0633ed134de880975134f57c05419b24cc2aa8
Vika Belaya <60939957+phhmaestro@users.noreply.github.com>
2020-11-01 13:24:50 -0700

    Forked BoardConfig.mk
    added exynos3

---
+++ BoardConfig.mk
@@ -0,0 +1,187 @@
+#
+
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/samsung/j1xlte
+
+# Include path
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+
+# Firmware
+TARGET_NO_BOOTLOADER := true
+
+BLOCK_BASED_OTA:= false
+
+# Platform
+TARGET_BOARD_PLATFORM := exynos3
+TARGET_SLSI_VARIANT := cm
+TARGET_SOC := exynos3475
+TARGET_BOOTLOADER_BOARD_NAME := universal3475
+
+# CPU
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a7
+
+# Extracted with libbootimg
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_IMAGE_NAME := zImage
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_CMDLINE += "androidboot.selinux=permissive"
+BOARD_KERNEL_SEPARATED_DT := true
+TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
+
+# Kernel
+TARGET_KERNEL_ARCH := arm
+TARGET_KERNEL_HEADER_ARCH := arm
+TARGET_USES_UNCOMPRESSED_KERNEL := true
+
+# Kernel config
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
+TARGET_KERNEL_SOURCE := kernel/samsung/j1xlte
+TARGET_KERNEL_CONFIG := lineage-j1xlte_defconfig
+BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
+
+# Partitions
+
+#Filesystem            1K-blocks    Used Available Use% Mounted on
+#rootfs                   851220    3176    848044   1% /
+#tmpfs                    934160     528    933632   1% /dev
+#tmpfs                    934160       0    934160   0% /mnt
+#/dev/block/mmcblk0p20   2975760  671072   2304688  23% /system
+#/dev/block/mmcblk0p21    197472     184    197288   1% /cache
+#/dev/block/mmcblk0p24  11467980 2948308   8519672  26% /data
+#/dev/block/mmcblk0p3      16048    2444     13604  16% /efs
+#/dev/block/mmcblk0p4       3952     548      3404  14% /cpefs
+#/dev/block/mmcblk0p18      8080     288      7792   4% /persdata/
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 13631488
+BOARD_CACHEIMAGE_PARTITION_SIZE := 209715200
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 15728640
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2415919104
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 4970250240
+BOARD_FLASH_BLOCK_SIZE := 4096
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE  := ext4
+BOARD_HAS_LARGE_FILESYSTEM := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_HAVE_SAMSUNG_BLUETOOTH := true
+BOARD_CUSTOM_BT_CONFIG := $(LOCAL_PATH)/bluetooth/libbt_vndcfg.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
+
+# Samsung HALs
+TARGET_AUDIOHAL_VARIANT := samsung
+TARGET_POWERHAL_VARIANT := samsung
+
+# Samsung Hardware
+BOARD_HARDWARE_CLASS += $(LOCAL_PATH)/cmhw
+BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
+
+# Samsung Camera
+BOARD_USE_SAMSUNG_CAMERAFORMAT_NV21 := true
+TARGET_HAS_LEGACY_CAMERA_HAL1 := true
+TARGET_NEEDS_LEGACY_CAMERA_HAL1_DYN_NATIVE_HANDLE := true
+BOARD_GLOBAL_CFLAGS += -DCAMERA_VENDOR_L_COMPAT
+TARGET_GLOBAL_CFLAGS += -DSAMSUNG_CAMERA_HARDWARE
+
+# Graphics
+USE_OPENGL_RENDERER := true
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+
+# HDMI
+BOARD_HDMI_INCAPABLE := true
+# FIMG2D
+BOARD_USES_SKIA_FIMGAPI := true
+# (G)SCALER
+BOARD_USES_SCALER := true
+# Samsung OpenMAX Video
+BOARD_USE_STOREMETADATA := true
+BOARD_USE_METADATABUFFERTYPE := true
+BOARD_USE_DMA_BUF := true
+BOARD_USE_ANB_OUTBUF_SHARE := true
+BOARD_USE_IMPROVED_BUFFER := true
+BOARD_USE_NON_CACHED_GRAPHICBUFFER := true
+BOARD_USE_GSC_RGB_ENCODER := true
+BOARD_USE_CSC_HW := false
+BOARD_USE_QOS_CTRL := false
+BOARD_USE_S3D_SUPPORT := true
+BOARD_USE_VP8ENC_SUPPORT := true
+
+# Bionic
+MALLOC_SVELTE := true
+
+# Video scaling issue workaround
+TARGET_OMX_LEGACY_RESCALING := true
+
+# Wifi
+BOARD_HAVE_SAMSUNG_WIFI          := true
+BOARD_WLAN_DEVICE                := bcmdhd
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcmdhd_sta.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcmdhd_apsta.bin"
+WIFI_BAND                        := 802_11_ABG
+
+BOARD_USES_HWC_SERVICES := true
+
+# Charger
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+CHARGING_ENABLED_PATH := /sys/class/power_supply/battery/batt_lp_charging
+
+# RIL
+BOARD_VENDOR := samsung
+BOARD_MODEM_TYPE := tss310
+BOARD_PROVIDES_LIBRIL := true
+
+# RIL.java overwrite
+BOARD_RIL_CLASS := ../../../$(LOCAL_PATH)/ril
+
+# Release tools
+TARGET_RELEASETOOLS_EXTENSIONS := $(LOCAL_PATH)
+
+# Dex-preoptimization to speed up boot sequences
+WITH_DEXPREOPT := true
+
+# Recovery
+BOARD_HAS_DOWNLOAD_MODE := true
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/ramdisk/fstab.universal3475
+
+# Init
+TARGET_INIT_VENDOR_LIB := libinit_j1xlte
+TARGET_RECOVERY_DEVICE_MODULES := libinit_j1xlte
+
+# Seccomp filters
+BOARD_SECCOMP_POLICY += $(LOCAL_PATH)/seccomp
+
+# SELinux
+BOARD_SEPOLICY_DIRS := $(LOCAL_PATH)/sepolicy
+
+# Inherit from the proprietary version
+-include vendor/samsung/j1xlte/BoardConfigVendor.mk
